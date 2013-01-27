7e1f1b5b713b1afd32eb0c908da0b3369281cdd6
mikebochenek <mike@bochenek.ca>
2013-01-27 18:29:06 +0100

    removed hardcoded 57 messages limit

--- src/main/java/com/vantaa3/RecommendationController.java
+++ src/main/java/com/vantaa3/RecommendationController.java
@@ -14,9 +14,6 @@ import javax.faces.bean.RequestScoped;
 @ManagedBean(name = "recommendationController")
 @RequestScoped
 public class RecommendationController {
-
-	private static final int MAX_RECOMMENDATIONS = 59; //TODO for now, I need to change this every time..
-
 	private Logger logger = Logger.getLogger(this.getClass().getName()); 
 
 	@EJB
@@ -25,12 +22,7 @@ public class RecommendationController {
 	private RankedRecommendationList list;
 	
 	public List<Recommendation> getAll() {
-		List<Recommendation> all = new ArrayList<Recommendation>();
-		for (int i = 1; i < MAX_RECOMMENDATIONS; i++) {
-			all.add(recommendationEJB.load(i));
-		}
-		
-		return all;
+		return recommendationEJB.loadAll();
 	}
 	
 	public List<String> getTags() {
@@ -73,8 +65,8 @@ public class RecommendationController {
 		long start = System.currentTimeMillis();
 		list = new RankedRecommendationList();
 		
-		for (int i = 1; i < MAX_RECOMMENDATIONS; i++) {
-			Recommendation r = recommendationEJB.load(i);
+		List<Recommendation> all = getAll();
+		for (Recommendation r : all) {
 			String tags[] = r.getTags().split(",");
 			for (String tag : tags) {
 				list.add(tag, r.getSentence(), r.getRating());
--- src/main/java/com/vantaa3/RecommendationEJB.java
+++ src/main/java/com/vantaa3/RecommendationEJB.java
@@ -1,10 +1,12 @@
 package com.vantaa3;
 
+import java.util.List;
 import java.util.logging.Logger;
 
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 @Stateless
 public class RecommendationEJB {
@@ -14,10 +16,16 @@ public class RecommendationEJB {
     @PersistenceContext(unitName = "chapter10PU")
     private EntityManager em;
 
+    
     public Recommendation load(int id) {
     	Recommendation r = em.find(Recommendation.class, id);
     	//logger.warning("loading  id:" + id + " r.getId(): " + (r==null ? "null" : r.getId()));
     	return r;
+    }
+    
+    public List<Recommendation> loadAll() {
+    	Query query = em.createQuery("select r from Recommendation r ");
+    	return query.getResultList();
     }
 
 }
