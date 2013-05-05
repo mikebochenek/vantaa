431b2fa2536662574b78cbe1bb8bd09d6c955027
mikebochenek <mike@bochenek.ca>
2013-05-05 16:26:36 +0200

    changing editor styling to custom

--- src/main/webapp/js/ckeditor/config.js
+++ src/main/webapp/js/ckeditor/config.js
@@ -1,28 +1,17 @@
-﻿/*
-Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
-For licensing, see LICENSE.html or http://ckeditor.com/license
-*/
+﻿CKEDITOR.editorConfig = function(config) {
+	config.uiColor = '#FAFAFA';
+	config.disableNativeSpellChecker = false;
 
-CKEDITOR.editorConfig = function( config )
-{
-	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
-	 config.uiColor = '#FFFFFF';
-	 
-	    config.toolbar = 'MyToolbar';
-
-	    config.toolbar_MyToolbar =
-	      [
-	       ['NewPage','Preview'],
-	       ['Cut','Copy','Paste','PasteText','PasteFromWord','-','Print', 'SpellChecker', 'Scayt'],
-	       ['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
-	       ['Maximize', 'ShowBlocks', 'Source'],
-	       '/',
-	       ['Styles','Format','Font','FontSize'],
-	       ['TextColor','BGColor'],
-	       ['Bold','Italic','Underline','Strike'],
-	       ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote'],
-	       ['JustifyLeft','JustifyCenter','JustifyBlock'],
-	       ['Link','Image','Table','Smiley']
-	   ];
+	config.toolbar = [
+			[ 'NewPage', 'Preview' ],
+			[ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Print', 'SpellChecker', 'Scayt' ],
+			[ 'Undo', 'Redo', '-', 'Find', 'Replace', '-', 'SelectAll', 'RemoveFormat' ],
+			[ 'Maximize', 'ShowBlocks', 'Source' ],
+			'/',
+			[ 'Styles', 'Format', 'Font', 'FontSize' ],
+			[ 'TextColor', 'BGColor' ],
+			[ 'Bold', 'Italic', 'Underline', 'Strike' ],
+			[ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', 'Blockquote' ],
+			[ 'JustifyLeft', 'JustifyCenter', 'JustifyBlock' ],
+			[ 'Link', 'Image', 'Table', 'Smiley' ] ];
 };
