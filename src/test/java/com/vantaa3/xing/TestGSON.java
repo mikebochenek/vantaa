0208399ec3448795ff2225e9a75aebc2169e33e8
Mike Bochenek <mike@bochenek.ca>
2013-12-31 15:08:08 +0100

    adding gson for fast json parsing

---
+++ src/test/java/com/vantaa3/xing/TestGSON.java
@@ -0,0 +1,14 @@
+package com.vantaa3.xing;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestGSON {
+	
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+	}
+
+}
