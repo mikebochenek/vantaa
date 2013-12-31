9f8541f5df96b027d108f3ba3800f12d1cbf7799
Mike Bochenek <mike@bochenek.ca>
2013-12-31 16:21:58 +0100

    cleanup XingRecommendationEJB and added logging to time request

--- src/main/java/com/vantaa3/xing/XingRecommendationController.java
+++ src/main/java/com/vantaa3/xing/XingRecommendationController.java
@@ -4,6 +4,7 @@
 package com.vantaa3.xing;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
@@ -12,30 +13,27 @@ import javax.faces.bean.RequestScoped;
 @ManagedBean(name = "xingRecommendationController")
 @RequestScoped
 public class XingRecommendationController {
+	private Logger logger = Logger.getLogger(this.getClass().getName()); 
 	
 	@EJB
 	private XingRecommendationEJB xingRecEJB;
 
 	private List<XingRecommendation> writtenRecommendations = null;
 	private List<XingRecommendation> receivedRecommendations = null;
-	
-	//TODO this test should be removed...
-	public XingRecommendation getOne() {
-		XingRecommendation rec = //xingRecEJB.load(0l);
-				xingRecEJB.loadByOwner("mike").get(0);
-				// xingRecEJB.loadBySubject("valentin").get(0);
-		if (rec == null) return new XingRecommendation();
-		return rec;
-	}
 
 	public int getWrittenRecommendationsCount() {
 		return getWrittenRecommendations().size();
 	}
 	
 	public List<XingRecommendation> getWrittenRecommendations() {
+		long startTS = System.currentTimeMillis();
 		if (writtenRecommendations == null) {
 			writtenRecommendations = xingRecEJB.loadByOwner("mike"); //TODO from session or something?
 		}
+		logger.info("writtenRecommendations loaded "
+				+ writtenRecommendations.size() + " recs in "
+				+ (System.currentTimeMillis() - startTS) + " ms");
+		
 		return writtenRecommendations;
 	}
 	
@@ -44,9 +42,14 @@ public class XingRecommendationController {
 	}
 
 	public List<XingRecommendation> getReceivedRecommendations() {
+		long startTS = System.currentTimeMillis();
 		if (receivedRecommendations == null) {
 			receivedRecommendations = xingRecEJB.loadBySubject("valentin"); //TODO from session or something?
 		}
+		logger.info("receivedRecommendations loaded "
+				+ receivedRecommendations.size() + " recs in "
+				+ (System.currentTimeMillis() - startTS) + " ms");
+		
 		return receivedRecommendations;
 	}
 
